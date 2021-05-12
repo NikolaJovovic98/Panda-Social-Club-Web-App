e3469c5b6587dc536f6064150b032b1280464ef5
nikolajovovic98 <dzonnna@gmail.com>
2021-05-12 18:34:24 +0200

    Google Drive Api Test

--- services/google_drive_api.js
+++ services/google_drive_api.js
@@ -2,8 +2,8 @@ const { google } = require("googleapis");
 const fs = require("fs");
 
 const oAuth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_DRIVE_CLIENT_ID,
-    process.env.GOOGLE_DRIVE_CLIENT_SECRET,
+    process.env.GOOGLE_DRIVE_CLIENT_ID ,
+    process.env.GOOGLE_DRIVE_CLIENT_SECRET ,
     process.env.GOOGLE_DRIVE_REDIRECT_URI
 );
 
